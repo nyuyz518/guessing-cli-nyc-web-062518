b3aa120b2a483dbc406592225091312379375527
Yun Zhang <YY@Yuns-MacBook-Air.local>
2018-06-05 14:06:46 -0400

    add guessing.rb

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,18 @@
 # Code your solution here!
+def guessing_cli
+  loop do
+    puts "Guess a number:" 
+    guess = gets.chomp
+    number = rand(1..n)
+    if guess == "exit"
+      puts "Goodbye"
+      break
+    elsif guess.to_i == number
+      puts "You guessed the correct number!"
+    elsif guess.to_i != number
+      puts "The computer guessed #{number}."
+    else
+      puts "Invalid"
+    end
+  end
+end
